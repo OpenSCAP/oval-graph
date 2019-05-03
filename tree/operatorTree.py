18c2a2698e9ea2d2e8f0b38e848c731e5ddf8fa1
Hony Rodák <hony.com@seznam.cz>
2019-05-03 17:57:58 +0200

    Added function for oval evaluation

--- tree/operatorTree.py
+++ tree/operatorTree.py
@@ -4,20 +4,25 @@ import operator
 class operatorTree(object):
     def __init__(self, node_id, value='root', children=None):
         self.node_id = node_id
-        self.value = value
+        allowedValues = ["or", "and", "one", "xor", "true", "false", "error", "unknown", "noteval", "notappl"]
+        value.lower()
+        if value in allowedValues:
+            self.value = value
+        else:
+            raise ValueError("err- unknown value: ", value)
         self.children = []
         if children is not None:
             for child in children:
                 self.add_child(child)
         else:
-            if self.value == "or" or self.value == "and":
-                raise ValueError('err- OR or AND have child!')
+            if self.value == "or" or self.value == "and" or self.value == "one" or self.value == "xor":
+                raise ValueError('err- OR, XOR, ONE, AND have child!')
 
     def __repr__(self):
         return self.value
 
     def add_child(self, node):
-        if self.value == "or" or self.value == "and":
+        if self.value == "or" or self.value == "and" or self.value == "one" or self.value == "xor":
             assert isinstance(node, operatorTree)
             self.children.append(node)
         else:
@@ -78,3 +83,98 @@ def addToTree(tree, node_id, newNode):
 
 def ChangeTreeValue(tree, node_id, value):
     findNodeWithID(tree, node_id).value = value
+
+
+result = {
+    'true_cnt': 1,
+    'false_cnt': 1,
+    'error_cnt': 1,
+    'unknown_cnt': 1,
+    'noteval_cnt': 1,
+    'notappl_cnt': 1
+}
+
+
+def counter(node):
+    result = {
+        'true_cnt': 0,
+        'false_cnt': 0,
+        'error_cnt': 0,
+        'unknown_cnt': 0,
+        'noteval_cnt': 0,
+        'notappl_cnt': 0
+    }
+
+    for child in node.children:
+        if child.value == 'true':
+            result['true_cnt'] += 1
+        elif child.value == 'false':
+            result['false_cnt'] += 1
+        elif child.value == 'error':
+            result['error_cnt'] += 1
+        elif child.value == 'unknown':
+            result['unknown_cnt'] += 1
+        elif child.value == 'noteval':
+            result['noteval_cnt'] += 1
+        elif child.value == 'notappl':
+            result['notappl_cnt'] += 1
+        else:
+            raise ValueError("err- unknown value:", child.value)
+
+
+def OVAL_OPERATOR_AND(result):
+    if result['true_cnt'] > 0 and result['false_cnt'] == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0:
+        outResult = 'OVAL_RESULT_TRUE'
+    elif result['false_cnt'] > 0:
+        outResult = 'OVAL_RESULT_FALSE'
+    elif result['false_cnt'] == 0 and result['error_cnt'] > 0:
+        outResult = 'OVAL_RESULT_ERROR'
+    elif result['false_cnt'] == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] > 0:
+        outResult = 'OVAL_RESULT_UNKNOWN'
+    elif result['false_cnt'] == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] > 0:
+        outResult = 'OVAL_RESULT_NOT_EVALUATED'
+    return outResult
+
+
+def OVAL_OPERATOR_ONE(result):
+    if result['true_cnt'] == 1 and result['false_cnt'] >= 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_TRUE'
+    elif result['true_cnt'] >= 2 and result['false_cnt'] >= 0 and result['error_cnt'] >= 0 and result['unknown_cnt'] >= 0 and result['noteval_cnt'] >= 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_FALSE'
+    elif result['true_cnt'] == 0 and result['false_cnt'] >= 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_FALSE'
+    elif result['true_cnt'] < 2 and result['false_cnt'] >= 0 and result['error_cnt'] > 0 and result['unknown_cnt'] >= 0 and result['noteval_cnt'] >= 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_ERROR'
+    elif result['true_cnt'] < 2 and result['false_cnt'] >= 0 and result['error_cnt'] == 0 and result['unknown_cnt'] >= 1 and result['noteval_cnt'] >= 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_UNKNOWN'
+    elif result['true_cnt'] < 2 and result['false_cnt'] >= 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] > 0 and result['notappl_cnt'] >= 0:
+        outResult = 'OVAL_RESULT_NOT_EVALUATED'
+    return outResult
+
+
+def OVAL_OPERATOR_OR(result):
+    if result['true_cnt'] > 0:
+        outResult = 'OVAL_RESULT_TRUE'
+    elif result['true_cnt'] == 0 and result['false_cnt'] > 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0:
+        outResult = 'OVAL_RESULT_FALSE'
+    elif result['true_cnt'] == 0 and result['error_cnt'] > 0:
+        outResult = 'OVAL_RESULT_ERROR'
+    elif result['true_cnt'] == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] > 0:
+        outResult = 'OVAL_RESULT_UNKNOWN'
+    elif result['true_cnt'] == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] > 0:
+        outResult = 'OVAL_RESULT_NOT_EVALUATED'
+    return outResult
+
+
+def OVAL_OPERATOR_XOR(result):
+    if (result['true_cnt'] % 2) == 1 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0:
+        outResult = 'OVAL_RESULT_TRUE'
+    elif (result['true_cnt'] % 2) == 0 and result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] == 0:
+        outResult = 'OVAL_RESULT_FALSE'
+    elif result['error_cnt'] > 0:
+        outResult = 'OVAL_RESULT_ERROR'
+    elif result['error_cnt'] == 0 and result['unknown_cnt'] > 0:
+        outResult = 'OVAL_RESULT_UNKNOWN'
+    elif result['error_cnt'] == 0 and result['unknown_cnt'] == 0 and result['noteval_cnt'] > 0:
+        outResult = 'OVAL_RESULT_NOT_EVALUATED'
+    return outResult

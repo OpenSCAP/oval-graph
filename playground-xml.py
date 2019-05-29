3586e1718b2176cb9242915c3ee23e13e7a19b4a
Jan Rodák <hony.com@seznam.cz>
2019-05-29 13:05:26 +0200

    Save work before launch :)

--- playground-xml.py
+++ playground-xml.py
@@ -7,34 +7,61 @@ pp = pprint.PrettyPrinter(indent=4)
 #Function for interpreting data
 def build_node(tree):
     node=dict(operator=tree.get('operator'),children=[])
-    print('OPERATOR: ', tree.get('operator'), tree.get('result'))
+    #print('OPERATOR: ', tree.get('operator'), tree.get('result'))
     for child in tree:
         if child.get('operator') is not None:
             node['children'].append(build_node(child))
         else:
             if child.get('definition_ref') is not None:
-                    print("extend_definition: ", child.get('result'))
-                    node['children'].append(dict(extend_definition=None))
+                    #print("extend_definition: ", child.get('result'))
+                    node['children'].append(dict(extend_definition = child.get('definition_ref')))
             else:
                 node['children'].append(dict(value_id=child.get('test_ref'),value=child.get('result')))
-                print('subTEST RES:',child.get('result'))
+                #print('subTEST RES:',child.get('result'))
     return node
 
 def build_tree(tree_data):
     test = dict(id=tree_data.get('definition_id'),tree=[])
 
-    print('TEST NAME: ', tree_data.get('definition_id'))
+    #print('TEST NAME: ', tree_data.get('definition_id'))
     for tree in tree_data:
         test['tree'].append(build_node(tree))
     
-    print(" ")
+    #print(" ")
     return test
 
+def fill_extend_definition(scan):
+    definitions=scan['definitions']
+    trees={}
+    out=dict(scan="none", definitions=[])
+    for definition in scan['definitions']:
+        #print(definition['id'])
+        for value in definition['tree']:
+            print(json.dumps(value,sort_keys=False, indent=4))
+            trees.append(operator_as_child(value,scan))
+            #break
+        out['definitions']=dict(id=definition['id'],tree=trees)
+        #print(definition['tree'][0]['children'])
+
+def operator_as_child(value):
+    out=dict(operator=value['operator'],children=[])
+    for child in value['children']:
+        #print(child['extend_definition'])
+        
+        if 'operator' in child:
+            operator_as_child(child)
+        else:
+            if 'extend_definition' in child:
+                print(json.dumps(child,sort_keys=False, indent=4))
+            
+           
+
 def run(trees_data):
-    scan=dict(scan="none",tests=[])
+    scan=dict(scan="none", definitions=[])
     for i in trees_data:
-        scan['tests'].append(build_tree(i))
-    print (json.dumps(scan,sort_keys=False, indent=4))
+        scan['definitions'].append(build_tree(i))
+    fill_extend_definition(scan)
+    #print (json.dumps(scan,sort_keys=False, indent=4))
     
 #parse data 
 def get_data_form_xml(src):

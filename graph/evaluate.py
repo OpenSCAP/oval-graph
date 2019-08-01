1f6e082b503fc54cff362678aa29c2a56555ff12
Hony Rodák <hony.com@seznam.cz>
2019-08-01 13:17:35 +0200

    Fix code style

--- graph/evaluate.py
+++ graph/evaluate.py
@@ -1,11 +1,19 @@
 """
     Function for evaluate oval operators.
+
+    AND operator - The AND operator produces a true result if every argument is true. If one or more arguments are     false, the result of the AND is false. If one or more of the arguments are unknown, and if none of              the arguments are false, then the AND operator produces a result of unknown.
+
+    ONE operator - The ONE operator produces a true result if one and only one argument is true. If there are more than argument is true (or if there are no true arguments), the result of the ONE is false. If one or more of the arguments are unknown, then the ONE operator produces a result of unknown.
+
+    OR operator - The OR operator produces a true result if one or more arguments is true. If every argument is false, the result of the OR is false. If one or more of the arguments are unknown and if none of arguments are true, then the OR operator produces a result of unknown.
+
+    XOR operator - XOR is defined to be true if an odd number of its arguments are true, and false otherwise. If any of the arguments are unknown, then the XOR operator produces a result of unknown.
 """
 
 
 def oval_operator_and(result):
     """
-        The AND operator produces a true result if every argument is true. If one or more arguments are false, the result of the AND is false. If one or more of the arguments are unknown, and if none of the arguments are false, then the AND operator produces a result of unknown.
+        AND operator
     """
     out_result = None
     if eq_zero(result, 'false_cnt')\
@@ -21,48 +29,44 @@ def oval_operator_and(result):
 
 def oval_operator_one(result):
     """
-        The ONE operator produces a true result if one and only one argument is true. If there are more than argument is true (or if there are no true arguments), the result of the ONE is false. If one or more of the arguments are unknown, then the ONE operator produces a result of unknown.
+        ONE operator
     """
     out_result = None
     if result['true_cnt'] == 1\
-            and result['false_cnt'] >= 0\
+            and eq_or_greater_zero(result, 'false_cnt')\
             and error_unknown_noteval_eq_zero(result)\
-            and result['notappl_cnt'] >= 0:
+            and eq_or_greater_zero(result, 'notappl_cnt'):
         out_result = 'true'
     elif result['true_cnt'] >= 2\
-            and result['false_cnt'] >= 0\
-            and result['error_cnt'] >= 0\
-            and result['unknown_cnt'] >= 0\
-            and result['noteval_cnt'] >= 0\
-            and result['notappl_cnt'] >= 0:
+            and eq_or_greater_zero(result, 'false_cnt')\
+            and eq_or_greater_zero(result, 'error_cnt')\
+            and eq_or_greater_zero_unknown_noteval_notappl(result):
         out_result = 'false'
     elif eq_zero(result, 'true_cnt')\
-            and result['false_cnt'] >= 0\
+            and eq_or_greater_zero(result, 'false_cnt')\
             and eq_zero(result, 'error_cnt')\
             and eq_zero(result, 'unknown_cnt')\
             and eq_zero(result, 'noteval_cnt')\
             and result['notappl_cnt'] >= 0:
         out_result = 'false'
     elif result['true_cnt'] < 2\
-            and result['false_cnt'] >= 0\
+            and eq_or_greater_zero(result, 'false_cnt')\
             and greater_zero(result, 'error_cnt')\
-            and result['unknown_cnt'] >= 0\
-            and result['noteval_cnt'] >= 0\
-            and result['notappl_cnt'] >= 0:
+            and eq_or_greater_zero_unknown_noteval_notappl(result):
         out_result = 'error'
     elif result['true_cnt'] < 2\
-            and result['false_cnt'] >= 0\
+            and eq_or_greater_zero(result, 'false_cnt')\
             and eq_zero(result, 'error_cnt')\
             and result['unknown_cnt'] >= 1\
-            and result['noteval_cnt'] >= 0\
-            and result['notappl_cnt'] >= 0:
+            and eq_or_greater_zero(result, 'noteval_cnt')\
+            and eq_or_greater_zero(result, 'notappl_cnt'):
         out_result = 'unknown'
     elif result['true_cnt'] < 2\
-            and result['false_cnt'] >= 0\
+            and eq_or_greater_zero(result, 'false_cnt')\
             and eq_zero(result, 'error_cnt')\
             and eq_zero(result, 'unknown_cnt')\
             and greater_zero(result, 'noteval_cnt')\
-            and result['notappl_cnt'] >= 0:
+            and eq_or_greater_zero(result, 'notappl_cnt'):
         out_result = 'noteval'
     else:
         out_result = None
@@ -71,7 +75,7 @@ def oval_operator_one(result):
 
 def oval_operator_or(result):
     """
-        The OR operator produces a true result if one or more arguments is true. If every argument is false, the result of the OR is false. If one or more of the arguments are unknown and if none of arguments are true, then the OR operator produces a result of unknown.
+        OR operator
     """
     out_result = None
     if greater_zero(result, 'true_cnt'):
@@ -87,18 +91,14 @@ def oval_operator_or(result):
 
 def oval_operator_xor(result):
     """
-        XOR is defined to be true if an odd number of its arguments are true, and false otherwise. If any of the arguments are unknown, then the XOR operator produces a result of unknown.
+        XOR operator
     """
     out_result = None
     if (result['true_cnt'] % 2) == 1\
-            and eq_zero(result, 'error_cnt')\
-            and eq_zero(result, 'unknown_cnt')\
-            and eq_zero(result, 'noteval_cnt'):
+            and eq_zero_unknown_noteval_notappl(result):
         out_result = 'true'
     elif (result['true_cnt'] % 2) == 0\
-            and eq_zero(result, 'error_cnt')\
-            and eq_zero(result, 'unknown_cnt')\
-            and eq_zero(result, 'noteval_cnt'):
+            and eq_zero_unknown_noteval_notappl(result):
         out_result = 'false'
     elif greater_zero(result, 'error_cnt'):
         out_result = 'error'
@@ -137,13 +137,37 @@ def and_or_eq_zero(operator, result):
         return eq_zero(result, 'true_cnt')
     return None
 
+
 def eq_zero(result, cnt):
     if result[cnt] == 0:
         return True
     return False
 
+
 def greater_zero(result, cnt):
     if result[cnt] > 0:
+        return True
+    return False
+
+
+def eq_or_greater_zero(result, cnt):
+    if result[cnt] >= 0:
+        return True
+    return False
+
+
+def eq_or_greater_zero_unknown_noteval_notappl(result):
+    if eq_or_greater_zero(result, 'unknown_cnt')\
+            and eq_or_greater_zero(result, 'noteval_cnt')\
+            and eq_or_greater_zero(result, 'notappl_cnt'):
+        return True
+    return False
+
+
+def eq_zero_unknown_noteval_notappl(result):
+    if eq_zero(result, 'error_cnt')\
+            and eq_zero(result, 'unknown_cnt')\
+            and eq_zero(result, 'noteval_cnt'):
         return True
     return False
 

ab044f502a1cd8593a763cb33c12424497d135a8
Hony Rodák <hony.com@seznam.cz>
2019-08-20 10:22:01 +0200

    Added Change description of client

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import graph.oval_graph
+import graph.xml_parser
 import json
 import argparse
 import webbrowser
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='client')
+    parser = argparse.ArgumentParser(description='Client for visualization scanned rule from Security scan.')
 
-    parser.add_argument("source_filename", help='arf scan file')
-    parser.add_argument("rule_name", help='Name of rule form scan')
+    parser.add_argument("source_filename", help='ARF scan file')
+    parser.add_argument("rule_name", help='Rule ID to be visualized. You can input part of ID rule.')
     parser.set_defaults(func=run)
 
     args = parser.parse_args()
@@ -18,6 +19,10 @@ def parse_arguments():
 
 def run(args):
     try:
+        xml_parser=graph.xml_parser.xml_parser(args.source_filename)
+        used_rules = xml_parser.get_used_rules()
+        
+
         oval_tree = graph.oval_graph.build_nodes_form_xml(
             args.source_filename, args.rule_name)
         with open('html_interpreter/data.js', "w+") as file:

e79be11298783faa9a97dcae2397ff565e4df568
Jan Rodak <hony.com@seznam.cz>
2020-04-09 15:50:44 +0200

    Fixes presentation of negation and simplifies methods

--- oval_graph/converter.py
+++ oval_graph/converter.py
@@ -33,7 +33,7 @@ class Converter():
         if isinstance(tree, OvalNode):
             self.tree = tree
             self.result = self.tree.evaluate_tree()
-            if not self.result:
+            if self.tree.node_type == 'value':
                 self.result = self.tree.value
         else:
             raise ValueError(
@@ -42,8 +42,8 @@ class Converter():
     def _get_node_icon(self):
         values = self._get_node_style()
         return dict(
-            color=self.VALUE_TO_BOOTSTRAP_COLOR[values['negation_color']],
-            icon=self.VALUE_TO_ICON[values['test_value']],
+            color=self.VALUE_TO_BOOTSTRAP_COLOR[values['test_value']],
+            icon=self.VALUE_TO_ICON[values['negation_color']],
         )
 
     def get_comment(self):
@@ -77,7 +77,7 @@ class Converter():
                    label=label['str'],
                    color_tag=self.BOOTSTRAP_COLOR_TO_LABEL_COLOR[icons['color']],
                    tag=self.get_tag(),
-                   result=self.result,
+                   result=self._get_not_negate_result(),
                    comment=self.get_comment()),
                "icon": icons['icon'],
                "state": {"opened": self._show_node(hide_passing_tests)},
@@ -88,16 +88,17 @@ class Converter():
                 hide_passing_tests) for child in self.tree.children]
         return out
 
+    def _get_not_negate_result(self):
+        if self.tree.negation and self.tree.node_type == 'operator' and self.is_bool(
+                self.result):
+            return self.negate_bool(self.result)
+        return self.result
+
     def _show_node(self, hide_passing_tests):
-        value = self.tree.evaluate_tree()
-        if value is None:
-            value = self.tree.value
-        if value == 'true' and hide_passing_tests:
-            return False
-        return True
+        return not(self.result == 'true' and hide_passing_tests)
 
     def _get_node_style(self):
-        value = self.result
+        value = self._get_not_negate_result()
         out_color = None
         if self.tree.negation and self.is_bool(value):
             out_color = self.negate_bool(value)
@@ -116,23 +117,27 @@ class Converter():
     def _get_label(self):
         out = dict(negation=None, str="")
         if self.tree.node_type == 'value':
-            if self.tree.negation:
-                out['negation'] = self.get_negation_character(self.tree.value)
+            out['negation'] = self._get_negation_for_label_of_value()
             out['str'] = re.sub(
                 '(oval:ssg-test_|oval:ssg-)|(:def:1|:tst:1)', '', str(self.tree.node_id))
-            return out
         else:
             if str(self.tree.node_id).startswith('xccdf_org'):
                 out['str'] = re.sub(
                     '(xccdf_org.ssgproject.content_)', '', str(
                         self.tree.node_id))
-                return out
             else:
-                if self.tree.negation:
-                    out['negation'] = self.get_negation_character(
-                        self.tree.evaluate_tree())
+                out['negation'] = self._get_negation_for_label_of_operator()
                 out['str'] = (self.tree.value).upper()
-                return out
+        return out
+
+    def _get_negation_for_label_of_value(self):
+        if self.tree.negation and self.is_bool(self.tree.value):
+            return self.get_negation_character(
+                self.negate_bool(self.tree.value))
+
+    def _get_negation_for_label_of_operator(self):
+        if self.tree.negation and self.is_bool(self.result):
+            return self.get_negation_character(self.result)
 
     def negate_bool(self, value):
         values = {
@@ -142,6 +147,4 @@ class Converter():
         return values[str(value)]
 
     def is_bool(self, value):
-        if value == "true" or value == "false":
-            return True
-        return False
+        return value == "true" or value == "false"

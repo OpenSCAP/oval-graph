8fb9742bf95dacfd5051302a052dd7677c1d37cf
Jan Rodak <hony.com@seznam.cz>
2020-08-18 15:25:41 +0200

    Implements option --all-in-one to json-to-graph command

--- oval_graph/json_to_html.py
+++ oval_graph/json_to_html.py
@@ -12,6 +12,7 @@ from .oval_node import restore_dict_to_tree
 from .converter import Converter
 from .exceptions import NotChecked
 
+
 class JsonToHtml(Client):
     def __init__(self, args):
         self.parser = None
@@ -22,7 +23,11 @@ class JsonToHtml(Client):
         self.source_filename = self.arg.source_filename
         self.rule_name = self.arg.rule_id
         self.out = self.arg.output
-        self.all_rules = self.arg.all
+        self.all_in_one = self.arg.all_in_one
+        if self.all_in_one:
+            self.all_rules = True
+        else:
+            self.all_rules = self.arg.all
         self.isatty = sys.stdout.isatty()
         self.show_failed_rules = False
         self.show_not_selected_rules = False
@@ -82,23 +87,51 @@ class JsonToHtml(Client):
         notselected_rules = []
         return self._check_rules_id(rules, notselected_rules)
 
-    def prepare_data(self, rules):
-        out = []
+    def create_dict_of_rule(self, rule):
+        self.oval_tree = self.load_json_to_oval_tree(rule)
+        return self.create_dict_of_oval_node(self.oval_tree)
+
+    def _prepare_all_in_one_data(self, rules, dict_oval_trees, out):
+        for rule in rules['rules']:
+            try:
+                dict_oval_trees[
+                    rule] = self.create_dict_of_rule(rule)
+            except NotChecked as error:
+                self.print_red_text(error)
+        src = self.get_save_src('rules')
+        self.save_html_with_all_rules_in_one(
+            dict_oval_trees, src, rules, out)
+        return out
+
+    def _prepare_data_by_one(self, rules, dict_oval_trees, out):
         for rule in rules["rules"]:
             try:
-                self.oval_tree = self.load_json_to_oval_tree(rule)
-                oval_tree_dict = self.create_dict_of_oval_node(
-                    self.oval_tree)
+                dict_oval_trees = self.create_dict_of_rule(rule)
                 src = self.get_save_src(
                     rule.replace('graph-of-', '') + "-")
                 self.save_html_and_open_html(
-                    oval_tree_dict, src, rule, out)
+                    dict_oval_trees, src, rule, out)
             except NotChecked as error:
                 self.print_red_text(error)
         return out
 
+    def prepare_data(self, rules):
+        out = []
+        dict_oval_trees = dict()
+        if self.all_in_one:
+            out = self._prepare_all_in_one_data(
+                rules, dict_oval_trees, out)
+        else:
+            out = self._prepare_data_by_one(rules, dict_oval_trees, out)
+        return out
+
     def prepare_parser(self):
         super().prepare_parser()
+        self.parser.add_argument(
+            '--all-in-one',
+            action="store_true",
+            default=False,
+            help="Processes all rules into one file.")
         self.parser.add_argument(
             '--off-web-browser',
             action="store_true",

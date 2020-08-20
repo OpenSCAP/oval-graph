ed4dc86a9e2db7bd90abcbabf23a0bd9fdd66241
Jan Rodak <hony.com@seznam.cz>
2020-08-20 16:33:39 +0200

    Removes unnecessary code to create html report in command arf-to-graph

--- oval_graph/arf_to_html.py
+++ oval_graph/arf_to_html.py
@@ -27,23 +27,11 @@ class ArfToHtml(Client):
         converter = Converter(self.xml_parser.get_oval_tree(rule_id))
         return converter.to_JsTree_dict(self.hide_passing_tests)
 
-    def _put_to_dict_oval_trees(self, dict_oval_trees, rule, date=None):
-        dict_oval_trees['graph-of-' + rule +
-                        date] = self.create_dict_of_rule(rule)
+    def _put_to_dict_oval_trees(self, dict_oval_trees, rule):
+        dict_oval_trees[rule] = self.create_dict_of_rule(rule)
 
     def _get_src_for_one_graph(self, rule, date=None):
         return self.get_save_src(rule + date)
-
-    def prepare_data(self, rules):
-        out = []
-        dict_oval_trees = dict()
-        date = str(datetime.now().strftime("-%d_%m_%Y-%H_%M_%S"))
-        if self.all_in_one:
-            out = self._prepare_all_in_one_data(
-                rules, dict_oval_trees, out, date)
-        else:
-            out = self._prepare_data_by_one(rules, dict_oval_trees, out, date)
-        return out
 
     def prepare_parser(self):
         super().prepare_parser()
